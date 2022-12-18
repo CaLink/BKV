5038a157a6f19fce24f92028876f99f4445b488d
KaliFLTK <kali@fltk.van>
2022-12-18 10:13:47 -0500

    Design Issue

--- BinCalc2/BinCalc.cxx
+++ BinCalc2/BinCalc.cxx
@@ -15,13 +15,13 @@ int main(int argc, char** argv) {
 		o->color((Fl_Color)23);
 		{
 			inputA = new Fl_Input(1060, aiY, 155, 40);
-     		inputA->value("0");
-     		inputA->callback((Fl_Callback*)GetInput);
+			inputA->value("0");
+			inputA->callback((Fl_Callback*)GetInput);
 			inputB = new Fl_Input(1060, biY, 155, 40);
-      		inputB->value("0");
+			inputB->value("0");
 			inputB->callback((Fl_Callback*)GetInput);
 			inputC = new Fl_Input(1060, ciY, 155, 40);
-      		inputC->value("0");
+			inputC->value("0");
 			inputC->callback((Fl_Callback*)GetInput);
 
 
@@ -81,7 +81,7 @@ int main(int argc, char** argv) {
 		}
 
 
-    init();
+		init();
 
 		o->end();
 	}
@@ -91,149 +91,161 @@ int main(int argc, char** argv) {
 
 void init()
 {
-  double scale[]={108.5714+25,37.3333+25,5.1612+25,15};
-  int x=0,w=0,t=0;
+	double scale[] = { 108.5714 + 25,37.3333 + 25,5.1612 + 25,15 };
+	int x = 0, w = 0, t = 0;
 
-  for(int i=0;i<4;i++)
-  {
-   w = i<3?10:0;
-   
-  
-    for(int j=0;j<pow(2,i+3);j++)
-  {
-    x = j*scale[i];
-    A[i][j] = new Fl_Button(65 + x,aY,15 + w,25,"0");
-    A[i][j]->labelsize(16);
-    A[i][j]->callback((Fl_Callback*)SetButtonValue);
-    A[i][j]->hide();
-    B[i][j] = new Fl_Button(65 + x,bY,15 + w,25,"0");
-    B[i][j]->labelsize(16);
-    B[i][j]->callback((Fl_Callback*)SetButtonValue);
-    B[i][j]->hide();
-    C[i][j] = new Fl_Button(65 + x,cY,15 + w,25,"0");
-    C[i][j]->labelsize(16);
-    C[i][j]->callback((Fl_Callback*)SetButtonValue);
-    C[i][j]->hide();
-  }
-  }
+	for (int i = 0; i < 4; i++)
+	{
+		w = i < 3 ? 10 : 0;
 
-// пример обращения к нужному массиву
 
-  for (int i = 0; i < bitArr[bit]; i++)
-  {
-    A[bitArr[bit+1]][i]->show();
-    B[bitArr[bit+1]][i]->show();
-    C[bitArr[bit+1]][i]->show();
-  }
-  
-  for (int i = 0; i < 12; i++)
-  {
-    opArr[i] = new Fl_Button(240 + 57.5*i,495,50,35,op[i]);
-    opArr[i]->callback((Fl_Callback*)DoOperation);
-  }
-  
+		for (int j = 0; j < pow(2, i + 3); j++)
+		{
+			x = j * scale[i];
+			A[i][j] = new Fl_Button(65 + x, aY, 15 + w, 25, "0");
+			A[i][j]->labelsize(16);
+			A[i][j]->callback((Fl_Callback*)SetButtonValue);
+			A[i][j]->hide();
+			B[i][j] = new Fl_Button(65 + x, bY, 15 + w, 25, "0");
+			B[i][j]->labelsize(16);
+			B[i][j]->callback((Fl_Callback*)SetButtonValue);
+			B[i][j]->hide();
+			C[i][j] = new Fl_Button(65 + x, cY, 15 + w, 25, "0");
+			C[i][j]->labelsize(16);
+			C[i][j]->callback((Fl_Callback*)SetButtonValue);
+			C[i][j]->hide();
+		}
+	}
+
+	// пример обращения к нужному массиву
+
+	for (int i = 0; i < bitArr[bit]; i++)
+	{
+		A[bitArr[bit + 1]][i]->show();
+		B[bitArr[bit + 1]][i]->show();
+		C[bitArr[bit + 1]][i]->show();
+	}
+
+	for (int i = 0; i < 12; i++)
+	{
+		opArr[i] = new Fl_Button(240 + 57.5 * i, 495, 50, 35, op[i]);
+		opArr[i]->callback((Fl_Callback*)DoOperation);
+	}
+
 
 }
 
-void SetButtonValue(Fl_Button *but,void*)
+void SetButtonValue(Fl_Button* but, void*)
 {
-	if (but->y()==cY)
+	if (but->y() == cY)
 		return;
-	but->label() == "1"? but->label("0") : but->label("1");
+	but->label() == "1" ? but->label("0") : but->label("1");
 
 	CountButton(but);
 }
 
-void CountButton(Fl_Button *but)
+void CountButton(Fl_Button* but)
 {
-	
+
 	switch (but->y())
 	{
-	case aY:Count(A[bitArr[bit+1]],inputA);break;
-	case bY:Count(B[bitArr[bit+1]],inputB);break;
-	//case cY:Count(C[bitArr[bit+1]],inputC);break;	
+	case aY:Count(A[bitArr[bit + 1]], inputA); break;
+	case bY:Count(B[bitArr[bit + 1]], inputB); break;
+		//case cY:Count(C[bitArr[bit+1]],inputC);break;	
 
 	}
-	
+
 }
 
-void Count(Fl_Button*arr[], Fl_Input *who)
+void Count(Fl_Button* arr[], Fl_Input* who)
 {
-	
-	char a= 0;
-	short b=0;
-	int c= 0;
-	int64_t d=0;
 
-	long long slider=1;
+	char a = 0;
+	short b = 0;
+	int c = 0;
+	long long d = 0;
 
-	for(int i = 0; i < bitArr[bit];i++)
+	long long slider = 1;
 
-
-	// все ещё не работает ссаные 64 бита
-	switch (bit)
-	{
-
-	case 0: a+= arr[i]->label() =="1"? 1<<(bitArr[bit]-1-i):0;break;
-	case 2:	b+= arr[i]->label() =="1"? 1<<(bitArr[bit]-1-i):0;break;
-	case 4:	c+= arr[i]->label() =="1"? 1<<(bitArr[bit]-1-i):0;break;
-	//case 6:	d+= arr[i]->label() =="1"? (long long)(slider<<(bitArr[bit]-1-i)):0;break;
-	case 6:	
-	int64_t temp = ((int64_t)(1)) << ((int64_t)(bitArr[bit] - 1 - i));
-	d+= arr[i]->label() =="1"? temp : 0;break;
+	for (int i = 0; i < bitArr[bit]; i++)
 
 
-	}
+		// все ещё не работает ссаные 64 бита
+		switch (bit)
+		{
 
-	if(who != NULL)
-	
-	switch (bit)
-	{
-	case 0: RePrint(a,who);break;
-	case 2:	RePrint(b,who);break;
-	case 4:	RePrint(c,who);break;
-	case 6:	RePrint(d,who);break;
-	}
+		case 0: 
+			a += arr[i]->label() == "1" ? slider << (bitArr[bit] - 1 - i) : 0; 
+			break;
+		case 2:
+			b += arr[i]->label() == "1" ? slider << (bitArr[bit] - 1 - i) : 0; 
+			break;
+		case 4:
+			c += arr[i]->label() == "1" ? slider << (bitArr[bit] - 1 - i) : 0; 
+			break;
+		case 6:
+			d += arr[i]->label() == "1" ? slider << (bitArr[bit] - 1 - i) : 0; 
+			break;
+
+		}
+
+	if (who != NULL)
+
+		switch (bit)
+		{
+		case 0: 
+			RePrint(a, who); break;
+		case 2:
+			RePrint(b, who); break;
+		case 4:
+			RePrint(c, who); break;
+		case 6:
+			RePrint(d, who, true); break;
+		}
 
 }
 
 void ChangeBit()
 {
 
-  for (int i = 0; i < bitArr[bit]; i++)
-  {
-    A[bitArr[bit+1]][i]->hide();
-    B[bitArr[bit+1]][i]->hide();
-    C[bitArr[bit+1]][i]->hide();
-  }
+	for (int i = 0; i < bitArr[bit]; i++)
+	{
+		A[bitArr[bit + 1]][i]->hide();
+		B[bitArr[bit + 1]][i]->hide();
+		C[bitArr[bit + 1]][i]->hide();
+	}
 
-  bit = bit==6?0:bit+2;
-  switch (bit)
-  {
-  case 0:bitChanger->label("8");break;
-  case 2:bitChanger->label("16");break;
-  case 4:bitChanger->label("32");break;
-  case 6:bitChanger->label("64");break;
-  
-  }
-  
-    for (int i = 0; i < bitArr[bit]; i++)
-  {
-    A[bitArr[bit+1]][i]->show();
-    B[bitArr[bit+1]][i]->show();
-    C[bitArr[bit+1]][i]->show();
-  }
+	bit = bit == 6 ? 0 : bit + 2;
+	switch (bit)
+	{
+	case 0:
+		bitChanger->label("8"); break;
+	case 2:
+		bitChanger->label("16"); break;
+	case 4:
+		bitChanger->label("32"); break;
+	case 6:
+		bitChanger->label("64"); break;
 
-  Count(A[bitArr[bit+1]],inputA);
-  Count(B[bitArr[bit+1]],inputB);
-  Count(C[bitArr[bit+1]],inputC);
-	
+	}
+
+	for (int i = 0; i < bitArr[bit]; i++)
+	{
+		A[bitArr[bit + 1]][i]->show();
+		B[bitArr[bit + 1]][i]->show();
+		C[bitArr[bit + 1]][i]->show();
+	}
+
+	Count(A[bitArr[bit + 1]], inputA);
+	Count(B[bitArr[bit + 1]], inputB);
+	Count(C[bitArr[bit + 1]], inputC);
+
 };
 
 void ChangeSystem()
 {
 	char str[50];
-	char sys[2]={'%'};
+	char sys[2] = { '%' };
 
 	/*switch (syst)
 	{
@@ -244,107 +256,137 @@ void ChangeSystem()
 
 	switch (syst)
 	{
-		case 8:	
-		syst=10;
+	case 8:
+		syst = 10;
 		sysChanger->label("10");
-		sprintf(str, "%d", cA);inputA->value((const char*)str);
-		sprintf(str, "%d", cB);inputB->value((const char*)str);
-		sprintf(str, "%d", cC);inputC->value((const char*)str);
+		sprintf(str, "%d", cA); inputA->value((const char*)str);
+		sprintf(str, "%d", cB); inputB->value((const char*)str);
+		sprintf(str, "%d", cC); inputC->value((const char*)str);
 		break;
-		case 10:
-		syst=16;
-		 sysChanger->label("16"); sys[1]='x';
-		sprintf(str, "%x", cA);inputA->value((const char*)str);
-		sprintf(str, "%x", cB);inputB->value((const char*)str);
-		sprintf(str, "%x", cC);inputC->value((const char*)str);
-		 break;
-		case 16:
-		syst=8;
-		sysChanger->label("8"); sys[1]='o';
-		sprintf(str, "%o", cA);inputA->value((const char*)str);
-		sprintf(str, "%o", cB);inputB->value((const char*)str);
-		sprintf(str, "%o", cC);inputC->value((const char*)str);
+	case 10:
+		syst = 16;
+		sysChanger->label("16"); sys[1] = 'x';
+		sprintf(str, "%x", cA); inputA->value((const char*)str);
+		sprintf(str, "%x", cB); inputB->value((const char*)str);
+		sprintf(str, "%x", cC); inputC->value((const char*)str);
+		break;
+	case 16:
+		syst = 8;
+		sysChanger->label("8"); sys[1] = 'o';
+		sprintf(str, "%o", cA); inputA->value((const char*)str);
+		sprintf(str, "%o", cB); inputB->value((const char*)str);
+		sprintf(str, "%o", cC); inputC->value((const char*)str);
 		break;
 	}
 
 }
 
-void SetValue(Fl_Input*who, Fl_Button*arr[])
+void SetValue(Fl_Input* who, Fl_Button* arr[])
 {
 
 	char str[50];
 	long long val = 0;
 
-		switch (syst)
+	switch (syst)
 	{
-	case 10: sscanf(who->value(),"%d",&val); break;
-	case 16: sscanf(who->value(),"%x",&val); break;
-	case 8: sscanf(who->value(),"%o",&val); break;
+	case 10: 
+		sscanf(who->value(), "%d", &val); break;
+	case 16:
+		sscanf(who->value(), "%x", &val); break;
+	case 8:
+		sscanf(who->value(), "%o", &val); break;
 	}
-	
-	for (int i = 0; i < bitArr[bit]; i++)
-	{
-		
-		(val&(1<<i))==0?arr[bitArr[bit]-1-i]->label("0"):arr[bitArr[bit]-1-i]->label("1");
-	}
-		
 
+	for (int i = 0; i < bitArr[bit]; i++)
+		(val & (1 << i)) == 0 ? arr[bitArr[bit] - 1 - i]->label("0") : arr[bitArr[bit] - 1 - i]->label("1");
 	
+}
+
+
+void SetValue(Fl_Input* who, Fl_Button* arr[], bool)
+{
+
+	char str[50];
+	long long val = 0;
+
+	switch (syst)
+	{
+	case 10: 
+		sscanf(who->value(), "%lld", &val); break;
+	case 16:
+		sscanf(who->value(), "%x", &val); break;
+	case 8: 
+		sscanf(who->value(), "%o", &val); break;
+	}
+
+	for (int i = 0; i < bitArr[bit]; i++)
+		(val & (((long long)(1)) << i)) == 0 ? arr[bitArr[bit] - 1 - i]->label("0") : arr[bitArr[bit] - 1 - i]->label("1");
 
 }
 
 void DoOperation(Fl_Button* but, void*)
 {
 
-	long long ans=0;
+	long long ans = 0;
 
-switch (but->x())
-{
-case 240:ans=cA+cB;	break;
-case 297:ans=cA-cB;	break;
-case 355:ans=cA*cB;	break;
-case 412:if(cB >0)ans=cA/cB;break;
-case 470:ans=cA%cB;	break;
-case 527:ans=cA<<cB;	break;
-case 585:ans=cA>>cB;	break;
-case 642:ans=~cA;	break;
-case 700:ans=~cB;	break;
-case 757:ans=cA&cB;	break;
-case 815:ans=cA|cB;	break;
-case 872:ans=cA^cB;	break;
-}  
+	switch (but->x())
+	{
+	case 240:
+		ans = cA + cB;	break;
+	case 297:
+		ans = cA - cB;	break;
+	case 355:
+		ans = cA * cB;	break;
+	case 412:
+		if (cB > 0)
+			ans = cA / cB; break;
+	case 470:
+		ans = cA % cB;	break;
+	case 527:
+		ans = cA << cB;	break;
+	case 585:
+		ans = cA >> cB;	break;
+	case 642:
+		ans = ~cA;	break;
+	case 700:
+		ans = ~cB;	break;
+	case 757:
+		ans = cA & cB;	break;
+	case 815:
+		ans = cA | cB;	break;
+	case 872:
+		ans = cA ^ cB;	break;
+	}
 
-switch (bit)
-{
-case 0: RePrint((char)ans,inputC);break;
-case 2: RePrint((short)ans,inputC);break;
-case 4: RePrint((int)ans,inputC);break;
-case 6: RePrint(ans,inputC);break;
+	switch (bit)
+	{
+	case 0: 
+		RePrint((char)ans, inputC);
+		SetValue(inputC, C[bitArr[bit + 1]]); break;
+	case 2: 
+		RePrint((short)ans, inputC);
+		SetValue(inputC, C[bitArr[bit + 1]]); break;
+	case 4: 
+		RePrint((int)ans, inputC); 
+		SetValue(inputC, C[bitArr[bit + 1]]); break;
+	case 6:
+		RePrint(ans, inputC, true); 
+		SetValue(inputC, C[bitArr[bit + 1]], true); break;
+
+	}
 
 }
 
-
-SetValue(inputC,C[bitArr[bit+1]]);
-
-/*
-switch (bit)
-{
-case 0:SetValue(inputC,C[0]) ;break;
-case 2:SetValue(inputC,C[1]) ;break;
-case 4:SetValue(inputC,C[2]) ;break;
-case 6:SetValue(inputC,C[3]) ;break;
-}
-*/
-
-}
-
-void GetInput(Fl_Input*inp)
+void GetInput(Fl_Input* inp)
 {
 	switch (inp->y())
 	{
-	case aiY:SetValue(inputA,A[bitArr[bit+1]]);break;
-	case biY:SetValue(inputB,B[bitArr[bit+1]]);break;
-	case ciY:RePrint(cC,inputC);break;
+	case aiY:
+		SetValue(inputA, A[bitArr[bit + 1]]); break;
+	case biY:
+		SetValue(inputB, B[bitArr[bit + 1]]); break;
+	case ciY:
+		RePrint(cC, inputC); break;
 	}
 }
 
--- BinCalc2/BinCalc.h
+++ BinCalc2/BinCalc.h
@@ -6,58 +6,99 @@
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Button.H>
 
-extern Fl_Input *inputA;
-extern Fl_Input *inputB;
-extern Fl_Input *inputC;
-extern Fl_Button *bitChanger;
-extern Fl_Button *sysChanger;
+extern Fl_Input* inputA;
+extern Fl_Input* inputB;
+extern Fl_Input* inputC;
+extern Fl_Button* bitChanger;
+extern Fl_Button* sysChanger;
 
 void init();
 
-void SetButtonValue(Fl_Button*,void*);
+void SetButtonValue(Fl_Button*, void*);
 void CountButton(Fl_Button*);
-void Count(Fl_Button*[],Fl_Input*);
+void Count(Fl_Button* [], Fl_Input*);
 //void RePrint
 void ChangeBit();
 void ChangeSystem();
-void DoOperation(Fl_Button* , void*);
-void SetValue(Fl_Input*,Fl_Button*[]);
+void DoOperation(Fl_Button*, void*);
+void SetValue(Fl_Input*, Fl_Button* []);
+void SetValue(Fl_Input* who, Fl_Button* arr[], bool);
 void GetInput(Fl_Input*);
 
 Fl_Button* opArr[12];
-const char *op[]={"+","-","*","/","%","<<",">>","~A","~B","&","|","^"};
+const char* op[] = { "+","-","*","/","%","<<",">>","~A","~B","&","|","^" };
 
-Fl_Button* A[4][64], *B[4][64], *C[4][64];
+Fl_Button* A[4][64], * B[4][64], * C[4][64];
 
-const int aY=265, bY = 385, cY = 590;   //для распознования ряда кнопок
-const int aiY=250,biY=375,ciY=575;
+const int aY = 265, bY = 385, cY = 590;   //для распознования ряда кнопок
+const int aiY = 250, biY = 375, ciY = 575;
 int bit = 4;                            //система равна 0-2-4-6
-int bitArr[8] = {8,0,16,1,32,2,64,3};   //система +1 - обращение к нужному массиву
+int bitArr[8] = { 8,0,16,1,32,2,64,3 };   //система +1 - обращение к нужному массиву
 int syst = 10;
 
-long long cA=0,cB=0,cC=0;
+long long cA = 0, cB = 0, cC = 0;
 
 template<class T>
-void RePrint(T ans,Fl_Input* who)
+void RePrint(T ans, Fl_Input* who)
 {
-    char str[50];
+	char str[50];
 
-    switch (syst)
-    {
-    case 8: sprintf(str,"%o",ans); who->value((const char*)str);break;
-    case 10: sprintf(str,"%d",ans);who->value((const char*)str);break;
-    case 16:sprintf(str,"%x",ans);who->value((const char*)str);break;
+	switch (syst)
+	{
+	case 8: 
+        sprintf(str, "%o", ans); 
+        who->value((const char*)str); break;
+	case 10: 
+        sprintf(str, "%d", ans); 
+        who->value((const char*)str); break;
+	case 16:
+        sprintf(str, "%x", ans); 
+        who->value((const char*)str); break;
 
-    }
+	}
 
-    switch (who->y())
-    {
-    case aiY: cA=ans; break;
-    case biY: cB=ans; break;
-    case ciY: cC=ans; break;
-        
-    }
-    
+	switch (who->y())
+	{
+	case aiY: 
+        cA = ans; break;
+	case biY: 
+        cB = ans; break;
+	case ciY: 
+        cC = ans; break;
+	}
+
+
+}
+template<class T>
+void RePrint(T ans, Fl_Input* who, bool q)
+{
+	char str[50];
+
+	switch (syst)
+	{
+	case 8: 
+        sprintf(str, "%o", ans);
+        who->value((const char*)str); break;
+	case 10:
+        sprintf(str, "%lld", ans);
+        who->value((const char*)str); break;
+	case 16:
+        sprintf(str, "%x", ans);
+        who->value((const char*)str); break;
+
+	}
+
+	switch (who->y())
+	{
+	case aiY: 
+        cA = ans; break;
+	case biY:
+        cB = ans; break;
+	case ciY:
+        cC = ans; break;
+
+	}
+
 
 }
 
